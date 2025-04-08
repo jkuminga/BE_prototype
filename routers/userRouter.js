3c154a6582c3a4ce7b60485058fd4748a73c791f
Your Name <you@example.com>
2025-04-08 11:38:05 +0900

    login

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const user = require('../lib/user');
+const db = require('../lib/db');
 
 router.post('/signup',(req, res)=>{
     user.sign_up(req, res);
@@ -9,4 +10,6 @@ router.post('/signup',(req, res)=>{
 router.post('/login', (req, res)=>{
     user.login(req, res);
 })
+
+
 module.exports = router;
