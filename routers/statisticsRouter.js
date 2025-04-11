e4d7abb32a5586bc690415da4344fa30b2fbdd7e
Your Name <you@example.com>
2025-04-11 20:50:52 +0900

    refactory codes

--- routers/statisticsRouter.js
+++ routers/statisticsRouter.js
@@ -2,4 +2,10 @@ const express = require('express')
 const router = express.Router();
 const ss = require('../lib/statistics')
 
+router.get('/logs/:id', (req, res)=>{
+    ss.get_logs(req, res);
+})
+
+
+
 module.exports = router;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,9 +11,18 @@ router.post('/login', (req, res)=>{
     user.login(req, res);
 })
 
-router.get('/getuserdata/:id/:name', (req, res)=>{
+router.post('/find/id', (req, res)=>{
+    user.find_id(req, res);
+})
+
+router.post('/find/pw', (req, res)=>{
+    user.find_pw(req, res);
+})
+
+router.get('/data/:id', (req, res)=>{
     user.get_user_data(req, res);
 })
+
 
 
 module.exports = router;
