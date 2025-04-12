d92aeea721ede392294f35fc60183d8ac6995783
Your Name <you@example.com>
2025-04-12 10:44:54 +0900

    management

---
+++ routers/manageRouter.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router();
+const manage = require('../lib/manage');
+
+router.post('/login', (req, res)=>{
+    manage.login(req, res);
+})
+
+router.post('/find/id', (req, res)=>{
+    manage.find_id(req, res);
+})
+
+router.post('/find/pw', (req, res)=>{
+    manage.find_pw(req, res);
+})
+
+
+
+module.exports = router;
